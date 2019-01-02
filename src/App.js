8809053fa3195a21b8da50eb95abcb313632ff9e
Sophanarith Sok <sok213@Alien-Macbook-Pro.local>
2019-01-01 22:57:12 -0800

    Create component files ;

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import MainModule from './components/MainModule/MainModule';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<MainModule />
+				<Footer /> 
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/MainModule/MainModule.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class MainModule extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Main Module.</h1>
+            </div>
+        )
+    }
+}
+
+export default MainModule;
